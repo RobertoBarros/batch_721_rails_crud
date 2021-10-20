83513085a8ade45c757b948f0aead8bf5ec5ae5a
Roberto Barros <roberto.f.barros@gmail.com>
2021-10-20 12:00:53 -0300

    utiliza rotas manuais

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,24 @@ Rails.application.routes.draw do
   # resources :users
   # resources :restaurants, except: [:destroy]
   # resources :restaurants, only: [:new, :create]
-  resources :restaurants
+  # resources :restaurants
 
-  # # INDEX
-  # get "restaurants", to: 'restaurants#index'
+  # INDEX
+  get "restaurants", to: 'restaurants#index'
 
-  # # CREATE
-  # get "restaurants/new", to: 'restaurants#new', as: 'new_restaurant'
-  # post "restaurants", to: 'restaurants#create'
+  # CREATE
+  get "restaurants/new", to: 'restaurants#new', as: 'new_restaurant'
+  post "restaurants", to: 'restaurants#create'
 
-  # # SHOW
-  # get "restaurants/:id", to: 'restaurants#show', as: 'restaurant'
+  # SHOW
+  get "restaurants/:id", to: 'restaurants#show', as: 'restaurant'
 
-  # # UPDATE
-  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
-  # patch 'restaurants/:id', to: 'restaurants#update'
+  # UPDATE
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  patch 'restaurants/:id', to: 'restaurants#update'
 
-  # # DESTROY
-  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  # DESTROY
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 
   root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
